a32a47898b017fb070b70b2c1acfb8736aca44e7
TheophilusM <theotakumug@gmail.com>
2022-04-17 18:42:46 +0200

    using appication config variables

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,15 +1,19 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 // return a response body always, not JSPs etc
 @RestController
 public class HelloController {
+    // using a  value saved in the config file
+    @Value("${welcome.message}")
+    private String welcomeMessage;
 
     // @RequestMapping(value = "/", method = RequestMethod.GET)
     @GetMapping("/")
     public String HelloWorld() {
-        return "Welcome to Spring Boot Server!";
+        return welcomeMessage;
     }
 }
